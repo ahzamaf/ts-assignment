d45cc774b99d8e1265f196982fd8e36151cf9b96
ahzamaf <165181358+ahzamaf@users.noreply.github.com>
2024-03-29 20:04:49 +0500

    Add files via upload

---
+++ no_users[ex#30].ts
@@ -0,0 +1,9 @@
+let userNames :string[]=['maaz', 'ali', 'ahzam', 'daniyal','ather']
+
+if(userNames.length === 0){
+    console.log('we need to find some users!')
+}
+else{
+    userNames = [];
+    console.log('all user name have been removed' + userNames.length)
+}
